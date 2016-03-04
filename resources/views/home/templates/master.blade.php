f424d62feca86bd4b28afcea1befcb201f891ba8
Matt Crandell <mrcrandell@gmail.com>
2016-03-03 21:59:50 -0500

    Updated styling to fix ios.

--- resources/views/home/templates/master.blade.php
+++ resources/views/home/templates/master.blade.php
@@ -38,7 +38,6 @@
         <![endif]-->
     </head>
     <body class="{{isset($idea)?'idea'.$idea:''}}">
-    <div  class="mobile-bg"></div>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
